411fa7c6a7a2ef18c1ea4a8b90ae3987fca34003
crazyma-nn <iwn001bim0@gmail.com>
2022-09-11 17:16:00 +0900

    github pages公開テスト

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form id="test">
+<input type="email" name="email" placeholder="email" aria-label="email"></input>
+        </form>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
