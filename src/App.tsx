5a8ca3f547c4841dc286b10aca5c53ddba92c3c2
crazyma-nn <iwn001bim0@gmail.com>
2022-09-17 21:35:26 +0900

    デザインを寄せる

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import Typewriter from 'typewriter-effect';
+
 
 function App() {
+ const typewriter = (<Typewriter
+  options={{
+    strings: ['I\'m maezmac.'],
+    autoStart: true,
+    loop: true,
+  }}
+/>);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form id="test">
-<input type="email" name="email" placeholder="email" aria-label="email"></input>
+        {typewriter}
+        <form id="mainForm">
+          <div className="inputform">
+            <input type="email"  name="email" placeholder="email" aria-label="email"></input>
+          </div>
+          <div className="inputform">
+            <input type="password" name="password" placeholder="password" aria-label="password"></input>
+          </div>
         </form>
-        <p>
-          I'm maezmac.
-        </p>
-        <a
-          className="App-link"
-          href="https://twitter.com/maezmac"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
+        <div id="forgot">
+          Forgot me?
+        </div>
+        <button>Sign In</button>
+        <div id="signUp">
+          Sign Up?
+        </div>
       </header>
     </div>
   );
